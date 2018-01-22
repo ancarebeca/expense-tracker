74994d35a70b1f215a4e2747d47df4b4d1cd578d
ancarebeca <anca.rebeca@gmail.com>
2018-01-22 17:20:23 +0000

    Refactor etl integration test

--- etl/etl_test.go
+++ etl/etl_test.go
@@ -13,18 +13,35 @@ import (
 )
 
 var (
-	db *sql.DB
+	db             *sql.DB
+	conf           config.Conf
+	confPath       = "../config/config_dev.yaml"
+	dataSourceName string
+	err            error
 )
 
-func Test_etl(t *testing.T) {
-	conf := config.Conf{}
-	conf.LoadConfig("../config/config_test.yaml")
+func Test_etl_extractFromCsv_transformData_loadIntoDatabase(t *testing.T) {
+	etl := createEtl()
+	etl.Run()
+	dbStatements := getStatementsFromDb()
+	assert.Equal(t, 36, len(dbStatements))
+	assert.Equal(t, "supermarket", dbStatements[0].TransactionDescription)
+	assert.Equal(t, 19.2, dbStatements[0].DebitAmount)
+	assert.Equal(t, 925.12, dbStatements[0].Balance)
+	assert.Equal(t, "2016-07-29", dbStatements[0].TransactionDate)
+	assert.Equal(t, float64(0), dbStatements[0].CreditAmount)
+	cleanDb()
+}
 
-	dataSourceName := fmt.Sprintf("%s:%s@/%s?charset=utf8", conf.UserDb, conf.PassDb, conf.Database)
-	db, err := sql.Open("mysql", dataSourceName)
+func createEtl() Etl {
+	conf = config.Conf{}
+	conf.LoadConfig(confPath)
+	dataSourceName = fmt.Sprintf("%s:%s@/%s?charset=utf8", conf.UserDb, conf.PassDb, conf.Database)
+	db, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
 	r := CsvReader{}
 
 	repository := repository.RepositoryDb{
@@ -42,7 +59,7 @@ func Test_etl(t *testing.T) {
 		CategoryFile: conf.CategoryPath,
 	}
 
-	etl := Etl{
+	return Etl{
 		conf,
 		&r,
 		&dt,
@@ -50,14 +67,14 @@ func Test_etl(t *testing.T) {
 		&c,
 		&l,
 	}
-	etl.Run()
+}
 
-	//query
+func getStatementsFromDb() []*model.Statement {
 	dbStatements := []*model.Statement{}
-
 	rows, err := db.Query("SELECT * FROM statements")
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Error when getting Statements: ", err.Error())
+		cleanDb()
 	}
 
 	for rows.Next() {
@@ -86,6 +103,7 @@ func Test_etl(t *testing.T) {
 		)
 		if err != nil {
 			fmt.Println(err.Error())
+			cleanDb()
 		}
 
 		s := model.Statement{
@@ -99,16 +117,12 @@ func Test_etl(t *testing.T) {
 		}
 		dbStatements = append(dbStatements, &s)
 	}
-	assert.Equal(t, 36, len(dbStatements))
-	assert.Equal(t, "supermarket", dbStatements[0].TransactionDescription)
-	assert.Equal(t, 19.2, dbStatements[0].DebitAmount)
-	assert.Equal(t, 925.12, dbStatements[0].Balance)
-	assert.Equal(t, "2016-07-29", dbStatements[0].TransactionDate)
-	assert.Equal(t, float64(0), dbStatements[0].CreditAmount)
+	return dbStatements
+}
 
+func cleanDb() {
 	_, err = db.Exec(`TRUNCATE TABLE statements`)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
